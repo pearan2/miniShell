c3c62b689c356c797f872ea13a0fe6d1eaa1125e
Junhyun Park <junhypar@student.42seoul.kr>
2021-03-30 16:33:55 +0900

    env

--- builtIn/ft_env.c
+++ builtIn/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: honlee <honlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 16:43:48 by junhypar          #+#    #+#             */
-/*   Updated: 2021/03/30 16:29:26 by honlee           ###   ########.fr       */
+/*   Updated: 2021/03/30 16:33:46 by junhypar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,16 @@ void		ft_env(t_info *info, int fd[2])
 	while(info->env[i])
 	{
 		len = ft_strlen(info->env[i]);
-		write(info->fd_stdout, info->env[i], len);
-		write(info->fd_stdout, "\n", 1);	
+		if (info->is_print == 0)
+		{
+			write(info->fd_stdout, info->env[i], len);
+			write(info->fd_stdout, "\n", 1);	
+		}
+		else
+		{
+			write(1, info->env[i], len);
+			write(1, "\n", 1);	
+		}
 		i++;
 	}
 	write(fd[1], "0\n", 2);
