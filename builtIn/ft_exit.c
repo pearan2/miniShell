262be507880468a92abc61fe7f923fac3de13a83
Junhyun Park <junhypar@student.42seoul.kr>
2021-03-31 19:21:02 +0900

    fix ft_exit--->no_numberic_input

--- builtIn/ft_exit.c
+++ builtIn/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: junhypar <junhypar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 11:21:50 by junhypar          #+#    #+#             */
-/*   Updated: 2021/03/31 19:14:28 by junhypar         ###   ########.fr       */
+/*   Updated: 2021/03/31 19:20:22 by junhypar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,18 @@ static int		exit_support(t_info *info, int cnt)
 	return (1);
 }
 
-static void		exit_result_support(int is_print, int flag)
+static void		exit_result_support(t_info *info, int flag)
 {
-	if (is_print == 1)
+	if (info->is_print == 1)
 	{
 		if (flag == 0)
 			write(1, "exit\n", 5);
 		else if (flag == 2)
-			write(2, "bash: exit: aa: numeric argument required\n", 42);
+		{
+			write(2, "bash: exit: ", 12);
+			write(2, info->opt[1], ft_strlen(info->opt[1]));
+			write(2, ": numeric argument required\n", 28);
+		}
 		else if (flag == 3)
 		{
 			write(1, "exit\n", 5);
@@ -50,25 +54,25 @@ static void		exit_result(t_info *info, int flag, int fd[2])
 {
 	if (flag == 0)
 	{
-		exit_result_support(info->is_print, flag);
+		exit_result_support(info, flag);
 		write(fd[1], "0\n", 2);
 		exit(20);
 	}
 	else if (flag == 2)
 	{
-		exit_result_support(info->is_print, flag);
+		exit_result_support(info, flag);
 		write(fd[1], "255\n", 4);
 		exit(20);
 	}
 	else if (flag == 3)
 	{
-		exit_result_support(info->is_print, flag);
+		exit_result_support(info, flag);
 		write(fd[1], "1\n", 2);
 		exit(1);
 	}
 	else
 	{
-		exit_result_support(info->is_print, flag);
+		exit_result_support(info, flag);
 		write(fd[1], info->opt[1], ft_strlen(info->opt[1]));
 		write(fd[1], "\n", 1);
 		exit(20);
